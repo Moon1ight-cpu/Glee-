24aea2e25cec66de2e5f507f4c6f50015339d8ac
Андрей <moon1ight2013@yandex.ru>
2022-04-15 01:35:02 +0300

    Сверстан блок промо

--- app/js/main.js
+++ app/js/main.js
@@ -5,3 +5,13 @@ $(function () {
     })
 });
 
+const WEEK_PRODUCTS = [
+    { name: 'Wooden radio', price: '$29.00', image: 'images/products/week/product-1.png' },
+    { name: 'Wooden chair', price: '$29.00', image: 'images/products/week/product-2.png' },
+    { name: 'White chair', price: '$29.00', image: 'images/products/week/product-3.png' },
+    { name: 'Retro table', price: '$29.00', image: 'images/products/week/product-4.png' },
+    { name: 'Wooden radio', price: '$29.00', image: 'images/products/week/product-5.png' },
+    { name: 'Wooden chair', price: '$29.00', image: 'images/products/week/product-6.png' },
+    { name: 'White chair', price: '$29.00', image: 'images/products/week/product-7.png' },
+    { name: 'Retro table', price: '$29.00', image: 'images/products/week/product-8.png' },
+]
