2dc35aa5bdef244d2f44936da7445a98f0c64481
Андрей <moon1ight2013@yandex.ru>
2022-04-18 04:54:11 +0300

    Добавлен шаблон хлебных крошек для побочных страниц

--- app/js/view.js
+++ app/js/view.js
@@ -6,14 +6,14 @@ export const DOM = {
 }
 
 export const WEEK_PRODUCTS = [
-    { name: 'Wooden radio', price: '$29.00', image: 'images/products/week/product-1.png' },
-    { name: 'Wooden chair', price: '$29.00', image: 'images/products/week/product-2.png' },
-    { name: 'White chair', price: '$29.00', image: 'images/products/week/product-3.png' },
-    { name: 'Retro table', price: '$29.00', image: 'images/products/week/product-4.png' },
-    { name: 'Wooden radio', price: '$29.00', image: 'images/products/week/product-5.png' },
-    { name: 'Wooden chair', price: '$29.00', image: 'images/products/week/product-6.png' },
-    { name: 'White chair', price: '$29.00', image: 'images/products/week/product-7.png' },
-    { name: 'Retro table', price: '$29.00', image: 'images/products/week/product-8.png' },
+    { id: 0, name: 'Wooden radio', price: '$29.00', image: 'images/products/week/product-1.png' },
+    { id: 1, name: 'Wooden chair', price: '$29.00', image: 'images/products/week/product-2.png' },
+    { id: 2, name: 'White chair', price: '$29.00', image: 'images/products/week/product-3.png' },
+    { id: 3, name: 'Retro table', price: '$29.00', image: 'images/products/week/product-4.png' },
+    { id: 4, name: 'Wooden radio', price: '$29.00', image: 'images/products/week/product-5.png' },
+    { id: 5, name: 'Wooden chair', price: '$29.00', image: 'images/products/week/product-6.png' },
+    { id: 6, name: 'White chair', price: '$29.00', image: 'images/products/week/product-7.png' },
+    { id: 7, name: 'Retro table', price: '$29.00', image: 'images/products/week/product-8.png' },
 ]
 
 export const NEWS = [
